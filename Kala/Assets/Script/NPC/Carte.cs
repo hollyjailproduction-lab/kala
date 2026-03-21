afb4eb83bf4f294bb2fe69d475faedda24eddc98
gakusahnama <hollyjailproduction@gmail.com>
2026-03-21 21:15:05 +0700

    dialog

--- Kala/Assets/Script/NPC/Carte.cs
+++ Kala/Assets/Script/NPC/Carte.cs
@@ -4,16 +4,39 @@ using System.Collections;
 
 public class Carte : MonoBehaviour
 {
-    public string[] dialogs;
-    public Sprite[] characterSprites; // opsional
+    public string[] firsttimeDialogs;
+    public Sprite[] firsttimeSprites;
+
+    public string[] repeatDialogs;
+    public Sprite[] repeatSprites;
+
     public bool playerIsClose;
+    private bool hasTalked = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
+            if (Input.GetKeyDown(KeyCode.F) && playerIsClose)
         {
-            DialogManager.Instance.StartDialog(dialogs, characterSprites);
-        }
+            if (DialogManager.Instance != null && DialogManager.Instance.IsDialogActive)
+                return;
+
+            string[] dialogsToUse;
+            Sprite[] spritesToUse;
+
+            if (!hasTalked)
+            {
+                dialogsToUse = firsttimeDialogs;
+                spritesToUse = firsttimeSprites;
+                hasTalked = true;
+            }
+            else
+            {
+                dialogsToUse = repeatDialogs;
+                spritesToUse = repeatSprites;
+            }
+
+            DialogManager.Instance.StartDialog(dialogsToUse, spritesToUse);
+        }       
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,7 +44,7 @@ public class Carte : MonoBehaviour
         if (other.CompareTag("Player"))
             playerIsClose = true;
     }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         playerIsClose = false;
