3254d019f6ece30a9e051ba719f1422391d55c82
gakusahnama <hollyjailproduction@gmail.com>
2026-04-17 11:22:26 +0700

    Dialog 2.0

---
+++ Kala/Assets/Script/Interface/Dialog/NPCDialogue.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewNPCDialogue", menuName = "NPC Dialogue")]
+public class NPCDialogue : ScriptableObject
+{
+    public string npcName;
+    public Sprite npcPortrait;
+    public string[] dialogueLines;
+    public float typingSpeed = 0.05f;
+    public bool[] autoProgressLines;
+    public float autoProgressDelay = 3f;
+}
